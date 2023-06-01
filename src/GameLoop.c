f9d606de89d9fe5949038fc52cad6a72ce5b20eb
Mainman002 <Mainman002@users.noreply.github.com>
2023-06-01 18:35:18 -0500

    Update window displayed name

--- src/GameLoop.c
+++ src/GameLoop.c
@@ -13,7 +13,7 @@ void GameLoop()
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("ChatGPT SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
