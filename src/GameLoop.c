70bc16dac03f02c0a687bae3dadc2dff776252ac
Mainman002 <Mainman002@users.noreply.github.com>
2023-06-01 21:50:13 -0500

    Update GameLoop.c
    Add Pong like Paddle player

--- src/GameLoop.c
+++ src/GameLoop.c
@@ -1,12 +1,19 @@
-// GameLoop.c
-
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "AssetManager.h"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 800;
+const int PADDLE_WIDTH = 128;
+const int PADDLE_HEIGHT = 24;
+const int PADDLE_SPEED = 5;
+double angle = 0.0;
+double rotationSpeed = 20.0; // 20 rotations per minute
+
+const int FRAME_DELAY = 1000 / 60;
+Uint32 frameStart;
+int frameTime;
 
 void GameLoop()
 {
@@ -17,7 +24,7 @@ void GameLoop()
     }
 
     // Create a window with VSync enabled
-    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -33,106 +40,110 @@ void GameLoop()
         return;
     }
 
-    // Load an image
-    SDL_Texture* texture = AssetManager_LoadTexture(renderer, "src/img/Ball_01.png");
-    if (texture == NULL) {
-        printf("Failed to load image!\n");
+    // Load the paddle texture
+    SDL_Texture* paddleTexture = AssetManager_LoadTexture(renderer, "src/img/Paddle.png");
+    if (paddleTexture == NULL) {
+        printf("Failed to load paddle texture!\n");
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return;
     }
 
-    // Get the image dimensions
-    int imgWidth, imgHeight;
-    SDL_QueryTexture(texture, NULL, NULL, &imgWidth, &imgHeight);
+    // Load the ball texture
+    SDL_Texture* ballTexture = AssetManager_LoadTexture(renderer, "src/img/Ball_01.png");
+    if (ballTexture == NULL) {
+        printf("Failed to load ball texture!\n");
+        SDL_DestroyTexture(paddleTexture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
 
-    // Set the destination rectangle for rendering the image
-    SDL_Rect destRect = {SCREEN_WIDTH / 2 - imgWidth / 2, SCREEN_HEIGHT / 2 - imgHeight / 2, imgWidth, imgHeight};
-
-    // Variables for rotation
-    double angle = 0.0;
-    double rotationSpeed = 20.0; // 20 rotations per minute
+    // Set the initial position of the paddle
+    int paddleX = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
+    int paddleY = SCREEN_HEIGHT - PADDLE_HEIGHT - 16;
 
     // Variables for timing
-    const int UPDATE_FPS = 60;
-    const int UPDATE_FRAME_DELAY = 1000 / UPDATE_FPS;
-    Uint32 updateFrameStart;
-    int updateFrameTime;
+    Uint32 frameStart;
+    int frameTime;
 
-    const int RENDER_FPS = 40;
-    const int RENDER_FRAME_DELAY = 1000 / RENDER_FPS;
-    Uint32 renderFrameStart;
-    int renderFrameTime;
+    // Variable to track whether the left or right arrow key is held down
+    bool moveLeft = false;
+    bool moveRight = false;
 
-    // Create an off-screen buffer for rendering
-    SDL_Texture* buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if (buffer == NULL) {
-        printf("Failed to create buffer texture!\n");
-        SDL_DestroyTexture(texture);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return;
-    }
-
-    // Update loop
+    // Game loop
     while (true) {
-        updateFrameStart = SDL_GetTicks();
+        frameStart = SDL_GetTicks();
 
         // Event handling
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
-                SDL_DestroyTexture(buffer);
-                SDL_DestroyTexture(texture);
+                SDL_DestroyTexture(paddleTexture);
                 SDL_DestroyRenderer(renderer);
                 SDL_DestroyWindow(window);
                 SDL_Quit();
                 return;
             }
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_LEFT)
+                    moveLeft = true;
+                else if (event.key.keysym.sym == SDLK_RIGHT)
+                    moveRight = true;
+            }
+            else if (event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_LEFT)
+                    moveLeft = false;
+                else if (event.key.keysym.sym == SDLK_RIGHT)
+                    moveRight = false;
+            }
         }
 
-        // Game logic update
-        double dt = 1.0 / UPDATE_FPS; // Delta time for game logic (60 FPS)
+        // Move the paddle based on the arrow key input
+        if (moveLeft && paddleX > 0) {
+            paddleX -= PADDLE_SPEED;
+        }
+        else if (moveRight && paddleX < SCREEN_WIDTH - PADDLE_WIDTH) {
+            paddleX += PADDLE_SPEED;
+        }
+
+        // Clear the renderer
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Render the paddle
+        SDL_Rect paddleRect = { paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT };
+        SDL_RenderCopy(renderer, paddleTexture, NULL, &paddleRect);
+
+        // Render the grid of rotating balls
+        int gridSizeX = 15;
+        int gridSizeY = 8;
+        int ballSize = 64;
+        int spacing = 20;
+
+        for (int row = 0; row < gridSizeY; row++) {
+            for (int col = 0; col < gridSizeX; col++) {
+                int ballX = col * (ballSize + spacing);
+                int ballY = row * (ballSize + spacing);
+                SDL_Rect ballRect = { ballX, ballY, ballSize, ballSize };
+                SDL_RenderCopyEx(renderer, ballTexture, NULL, &ballRect, angle * 180.0 / M_PI, NULL, SDL_FLIP_NONE);
+            }
+        }
+
+        // Update the angle for rotation
+        double dt = frameTime / 1000.0; // Delta time in seconds
+        double rotationAngle = (rotationSpeed * 2.0 * M_PI / 60.0) * dt;
+        angle += rotationAngle;
+
+        // Update the screen
+        SDL_RenderPresent(renderer);
 
         // Frame rate regulation
-        updateFrameTime = SDL_GetTicks() - updateFrameStart;
-        if (UPDATE_FRAME_DELAY > updateFrameTime) {
-            SDL_Delay(UPDATE_FRAME_DELAY - updateFrameTime);
-        }
-
-        // Render loop
-        renderFrameStart = SDL_GetTicks();
-        while (SDL_GetTicks() - renderFrameStart < RENDER_FRAME_DELAY) {
-            // Clear the buffer with the sky blue color
-            SDL_SetRenderTarget(renderer, buffer);
-            SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
-            SDL_RenderClear(renderer);
-
-            // Update rotation
-            angle += (rotationSpeed * 2.0 * M_PI / 60.0) * dt; // 20 rotations per minute
-
-            // Set the rotation center
-            SDL_Point rotationCenter = {imgWidth / 2, imgHeight / 2};
-
-            // Render the image with rotation
-            SDL_RenderCopyEx(renderer, texture, NULL, &destRect, angle * 180.0 / M_PI, &rotationCenter, SDL_FLIP_NONE);
-
-            // Set the render target back to the default window
-            SDL_SetRenderTarget(renderer, NULL);
-
-            // Copy the buffer to the window
-            SDL_RenderCopy(renderer, buffer, NULL, NULL);
-
-            // Update the screen
-            SDL_RenderPresent(renderer);
-
-            // Frame rate regulation
-            renderFrameTime = SDL_GetTicks() - renderFrameStart;
-            if (RENDER_FRAME_DELAY > renderFrameTime) {
-                SDL_Delay(RENDER_FRAME_DELAY - renderFrameTime);
-            }
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < FRAME_DELAY) {
+            SDL_Delay(FRAME_DELAY - frameTime);
         }
     }
 }
